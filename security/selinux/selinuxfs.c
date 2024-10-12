20efb98854c7487bc634674ae86db9f8c5597588
anruichan <anrui2032@gmail.com>
2024-10-12 19:44:50 +0800

    Partial revert ZTE change (selinux)
    Change-Id: I320a1e0bed77d3fc43002b0e15dbc989d1b99077

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -46,13 +46,6 @@
 #include "ss/policyproc.h"
 #endif /* CONFIG_SECURITY_SELINUX_POLICYPROC */
 
-#if defined(ZTE_FEATURE_TF_DEBUG)
-#define ZTE_TF_ENABLE_SELINUX
-#endif
-
-#if defined(ZTE_FEATURE_TF_PARTIAL)
-#define ZTE_TF_ENABLE_SELINUX
-#endif
 /* Policy capability filenames */
 static char *policycap_names[] = {
 	"network_peer_controls",
@@ -181,22 +174,6 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	if (sscanf(page, "%d", &new_value) != 1)
 		goto out;
 
-#if defined(ZTE_FEATURE_TF_DEBUG)
-#define ZTE_TF_ENABLE_ENFORCE
-#endif
-
-#if defined(ZTE_FEATURE_TF_PARTIAL)
-#define ZTE_TF_ENABLE_ENFORCE
-#endif
-
-#if defined(ZTE_FEATURE_TF_SECURITY_SYSTEM)
-#if !defined(ZTE_TF_ENABLE_ENFORCE)
-	if (selinux_enforcing == 1) {
-		length = -EPERM;
-		goto out;
-	}
-#endif
-#endif
 	if (new_value != selinux_enforcing) {
 		length = task_has_security(current, SECURITY__SETENFORCE);
 		if (length)
@@ -316,12 +293,6 @@ static ssize_t sel_write_disable(struct file *file, const char __user *buf,
 	if (*ppos != 0)
 		goto out;
 
-#if defined(ZTE_FEATURE_TF_SECURITY_SYSTEM)
-#if !defined(ZTE_TF_ENABLE_SELINUX)
-	if (ss_initialized)
-		return -EINVAL;
-#endif
-#endif
 	length = -ENOMEM;
 	page = (char *)get_zeroed_page(GFP_KERNEL);
 	if (!page)
@@ -564,12 +535,6 @@ static ssize_t sel_write_load(struct file *file, const char __user *buf,
 	if (count > 64 * 1024 * 1024)
 		goto out;
 
-#if defined(ZTE_FEATURE_TF_SECURITY_SYSTEM)
-#if !defined(ZTE_TF_ENABLE_SELINUX)
-	if (ss_initialized)
-		return -EINVAL;
-#endif
-#endif
 	length = -ENOMEM;
 	data = vmalloc(count);
 	if (!data)
